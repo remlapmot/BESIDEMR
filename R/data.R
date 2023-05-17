bd7f0ceab8bf16c9d74d9af7f021fb733fb94626
Tom Palmer <remlapmot@hotmail.com>
2023-05-17 17:07:20 +0100

    Fix http to https

--- R/data.R
+++ R/data.R
@@ -3,8 +3,8 @@
 #' We performed three-sample MR, where we selected instruments from The Metabolic
 #' Syndrome in Men (METSIM) GWAS, then extracted their summary data from `MRChallenge2019`, where:
 #' \enumerate{
-#' \item{SNP-HDL: }{Kettunen et al. Available from: \url{http://europepmc.org/articles/PMC4814583}}
-#' \item{SNP-AMD: }{International AMD Genomics Consortium (IAMDGC) Available from: \url{http://europepmc.org/articles/PMC4745342}}
+#' \item{SNP-HDL: }{Kettunen et al. Available from: \url{https://europepmc.org/articles/PMC4814583}}
+#' \item{SNP-AMD: }{International AMD Genomics Consortium (IAMDGC) Available from: \url{https://europepmc.org/articles/PMC4745342}}
 #' }
 #' @docType data
 #' @usage data(AMD_HDL)
