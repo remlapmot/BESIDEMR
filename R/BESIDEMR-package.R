9393b6ca1fac21fd75a1ebc87d464e3adedbab59
Tom Palmer <remlapmot@hotmail.com>
2023-05-17 11:40:08 +0100

    Create BESIDEMR-package.R

---
+++ R/BESIDEMR-package.R
@@ -0,0 +1,13 @@
+#' BESIDEMR: BayEsian Set IDEntification Mendelian randomization
+#'
+#' R package to perform BayEsian Set IDEntification Mendelian randomization (BESIDE-MR).
+#'
+#' @references
+#' Shapland, C.Y., Zhao Q, Bowden J, Profile-likelihood Bayesian model
+#' averaging for two-sample summary data Mendelian randomization in the presence
+#' of horizontal pleiotropy. Statistics in Medicine, 2022, 41, 6, 1100-1119.
+#' \doi{10.1002/sim.9320}
+#' @name BESIDEMR-package
+#' @docType package
+#' @aliases besidemr BESIDEMR
+NULL
