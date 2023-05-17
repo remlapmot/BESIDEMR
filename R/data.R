64ecdb239ed2392d08002c12038d468efcd438b3
Tom Palmer <remlapmot@hotmail.com>
2023-05-17 11:40:01 +0100

    Formatting

--- R/data.R
+++ R/data.R
@@ -1,9 +1,10 @@
 #' Effect of HDL cholesterol on age-related macular degeneration (AMD)
 #'
-#' We performed three-sample MR, where we selected instruments from The Metabolic Syndrome in Men (METSIM) GWAS, then extracted their summary data from `MRChallenge2019`, where:
+#' We performed three-sample MR, where we selected instruments from The Metabolic
+#' Syndrome in Men (METSIM) GWAS, then extracted their summary data from `MRChallenge2019`, where:
 #' \enumerate{
-#' \item{SNP-HDL:}{Kettunen et al. Available from: http://europepmc.org/articles/PMC4814583}
-#' \item{SNP-AMD:}{International AMD Genomics Consortium (IAMDGC) Available from: http://europepmc.org/articles/PMC4745342}
+#' \item{SNP-HDL: }{Kettunen et al. Available from: \url{http://europepmc.org/articles/PMC4814583}}
+#' \item{SNP-AMD: }{International AMD Genomics Consortium (IAMDGC) Available from: \url{http://europepmc.org/articles/PMC4745342}}
 #' }
 #' @docType data
 #' @usage data(AMD_HDL)
